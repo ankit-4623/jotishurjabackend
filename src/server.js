75cdc9bcb0d16fb232eda5ae7485c60b51e0014e
ankit-4623 <ankitnayek007@gmail.com>
2025-09-22 01:38:22 +0530

    update server.js

---
+++ src/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import productRoute from "./route/productRoute.js";
+import connectDB from "./config/db.js";
+import userRoute from "./route/userRoute.js";
+import cookieParser from "cookie-parser";
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
+//middlewares
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(cors());
+
+// routes
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
