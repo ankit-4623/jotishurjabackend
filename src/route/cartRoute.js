efaa456d232096eef58bdb5f28e83a70e854301e
ankit-4623 <ankitnayek007@gmail.com>
2025-09-22 21:51:12 +0530

    complete cart route.js

--- src/route/cartRoute.js
+++ src/route/cartRoute.js
@@ -1,7 +1,10 @@
 import express from "express";
-import { addTocart, fetchCart } from "../controllers/cartControllers.js";
+import { addTocart, clearCart, fetchCart, removeFromCart, updateCart } from "../controllers/cartControllers.js";
 const router = express.Router();
 
 router.post('/addTocart',addTocart)
+router.post('/removeFromcart/:id',removeFromCart)
+router.post('/clearCart',clearCart)
+router.put('/updateCart',updateCart)
 router.get('/fetchCart',fetchCart)
 export default router;
