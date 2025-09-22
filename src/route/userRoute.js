b890dff14b71b56422356178aee31582ff08cb10
ankit-4623 <ankitnayek007@gmail.com>
2025-09-22 15:51:28 +0530

    completed userRoute.js

--- src/route/userRoute.js
+++ src/route/userRoute.js
@@ -1,9 +1,16 @@
 import express from 'express';
-import { loginUser, logoutUser, registerUser } from '../controllers/userControllers.js';
+import { checkOtp, getUserProfile, loginUser, logoutUser, registerUser, resetPassord, sendOtp } from '../controllers/userControllers.js';
 const router = express.Router();
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
 router.route('/logout').post(logoutUser);
+router.route('/user').get(getUserProfile);
+
+// otp
+      
+router.route('/send-otp').post(sendOtp);
+router.route('/check-otp').post(checkOtp);
+router.route('/reset-password').post(resetPassord);
 
 export default router;
