09e4578c1be24376e5b37c64027ed4ba7cc7206b
ankit-4623 <ankitnayek007@gmail.com>
2025-09-22 13:10:34 +0530

    completed prodcutRoute.js

--- src/route/productRoute.js
+++ src/route/productRoute.js
@@ -2,10 +2,16 @@ import express from "express";
 import {
   addProduct,
   deleteProduct,
+  getAllProducts,
+  getProductById,
+  updateProduct,
 } from "../controllers/productControllers.js";
 import { upload } from "../config/multer.js";
 const router = express.Router();
 router.post("/addProduct", upload.array("images", 5), addProduct);
+router.get("/getproduct/:id",getProductById);
+router.get("/getallProduct",getAllProducts);
 router.delete("/deleteProduct/:id", deleteProduct);
+router.put("/updatedProduct/:id",upload.array("images", 5), updateProduct);
 
 export default router;
