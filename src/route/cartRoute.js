2c670b4610fd74632c83fa97e78327b2a467cb81
ankit-4623 <ankitnayek007@gmail.com>
2025-09-23 15:53:46 +0530

    updated cartRoute.js

--- src/route/cartRoute.js
+++ src/route/cartRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { addTocart, clearCart, fetchCart, removeFromCart, updateCart } from "../controllers/cartControllers.js";
+import { verifyUserAuth } from "../middlewares/userAuth.js";
 const router = express.Router();
 
-router.post('/addTocart',addTocart)
-router.post('/removeFromcart/:id',removeFromCart)
-router.post('/clearCart',clearCart)
-router.put('/updateCart',updateCart)
-router.get('/fetchCart',fetchCart)
+router.post('/addTocart',verifyUserAuth,addTocart)
+router.post('/removeFromcart/:id',verifyUserAuth,removeFromCart)
+router.post('/clearCart',verifyUserAuth,clearCart)
+router.put('/updateCart',verifyUserAuth,updateCart)
+router.get('/fetchCart',verifyUserAuth,fetchCart)
 export default router;
