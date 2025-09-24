ee722100feb7344b3548c798a76d53afd2aa8723
ankit-4623 <ankitnayek007@gmail.com>
2025-09-24 12:43:43 +0530

    completed addressRoute.js

--- src/route/addressRoute.js
+++ src/route/addressRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { Addaddress, deleteAddress, editAddress, fetchAddress } from "../controllers/addressControllers.js";
+import { verifyUserAuth } from "../middlewares/userAuth.js";
 const router = express.Router();
-router.post('/addAddress',Addaddress)
-router.get('/getaddress/:id',fetchAddress)
-router.delete("/delete/:userId/:addressId", deleteAddress);
-router.put("/update/:userId/:addressId", editAddress);
+router.post('/addAddress',verifyUserAuth,Addaddress)
+router.get('/getaddress/:id',verifyUserAuth,fetchAddress)
+router.delete("/delete/:userId/:addressId",verifyUserAuth, deleteAddress);
+router.put("/update/:userId/:addressId", verifyUserAuth,editAddress);
 
 export default router;
