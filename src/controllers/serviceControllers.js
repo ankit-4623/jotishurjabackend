11b754af96e41d362dcfbbdd95f4f0560436508d
ankit-4623 <ankitnayek007@gmail.com>
2025-10-11 21:20:05 +0530

    updated serviceControllers.js

--- src/controllers/serviceControllers.js
+++ src/controllers/serviceControllers.js
@@ -1,32 +1,64 @@
 import axios from "axios";
+import { json } from "express";
 export const dailyhoroscope = async (req, res) => {
   try {
     const { sign } = req.query;
     const data = `https://api.api-ninjas.com/v1/horoscope?zodiac=${sign}`;
-   
+
     const response = await axios.get(data);
 
-    res.status(200).json({ message: "dailyhoroscope controller", data: response.data });
+    res
+      .status(200)
+      .json({ message: "dailyhoroscope controller", data: response.data });
   } catch (error) {
     console.error("Error in dailyhoroscope controller:", error);
     res.status(500).json({ error: "dailyhoroscope Internal Server Error" });
   }
 };
 
+
+
 export const matchmaking = async (req, res) => {
   try {
-    res.status(200).json({ message: "matchmaking controller" });
+    const { male, female } = req.body;
+ 
+
+    const { data } = await axios.post(
+      "https://json.freeastrologyapi.com/match-making/ashtakoot-score",
+      {
+        male,
+        female,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "x-api-key": process.env.MATCHMAKING_API_KEY,
+        },
+      }
+    );
+
+    res.status(200).json({
+      message: "Matchmaking success",
+      data,
+    });
   } catch (error) {
-    console.error("Error in matchmaking controller:", error);
-    res.status(500).json({ error: "matchmaking Internal Server Error" });
+    console.error(
+      "Error in matchmaking controller:",
+      error.response?.data || error.message
+    );
+    res.status(500).json({
+      error: "Matchmaking Internal Server Error",
+      details: error.response?.data || error.message,
+    });
   }
 };
 
+
 export const freekundli = async (req, res) => {
-      try {
-        res.status(200).json({ message: "freekundli controller" });
-      } catch (error) {
-        console.error("Error in freekundli controller:", error);
-        res.status(500).json({ error: "freekundli Internal Server Error" });
-      }
+  try {
+    res.status(200).json({ message: "freekundli controller" });
+  } catch (error) {
+    console.error("Error in freekundli controller:", error);
+    res.status(500).json({ error: "freekundli Internal Server Error" });
+  }
 };
